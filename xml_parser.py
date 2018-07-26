998687482dd0bf11c07415122e468b3076c590cd
Vitalii Krashchenko <vitalii.krashchenko@kohls.com>
2018-07-26 23:45:10 +0200

    added python parser

--- xml_parser.py
+++ xml_parser.py
@@ -1,10 +1,18 @@
 import xml.etree.ElementTree as ET
-
-tree = ET.parse('./Sprint_6968.xml')
-root = tree.getroot()
-for items in root.iter('item'):
-    if "Sub-task" not in items.find('type').text:
+def parseExport(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    for items in root.iter('item'):
+        #if "Sub-task" not in items.find('type').text:
         jira_id = items.find('key').text
         assigne = items.find('assignee').text
-        epic = items.find('customfieldfields').find('customfieldvalue')
-        print('{} * {} :: {}'.format(epic, jira_id, assigne))
+        # epic2 = items.find('customfields')
+        epic = items.find('customfields')[5][1][0].text
+        if "OCFD" not in epic:
+            epic = items.find('customfields')[6][1][0].text
+            print('{},{},{}'.format(epic, jira_id, assigne))
+        else:
+            print('{},{},{}'.format(epic, jira_id, assigne))
+
+for i in ['./done.xml']:
+    parseExport(i)
